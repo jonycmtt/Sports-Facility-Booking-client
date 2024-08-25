3a45c961582f2a8eff96b6db487b2f5b791bf5b2
jonycmtt <jonyislamcmt@gmail.com>
2024-08-26 01:42:22 +0600

    [done] how to works section completed

--- src/pages/Landing/LandingPages.tsx
+++ src/pages/Landing/LandingPages.tsx
@@ -1,9 +1,11 @@
 import Header from "./Headers/Header";
+import HowItWorks from "./Headers/Works/HowItWorks";
 
 const LandingPages = () => {
   return (
     <div>
       <Header />
+      <HowItWorks />
     </div>
   );
 };
