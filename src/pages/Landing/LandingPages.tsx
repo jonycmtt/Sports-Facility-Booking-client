b98d4646c5146109026a5e2c9aa34d7e6a3d935f
jonycmtt <jonyislamcmt@gmail.com>
2024-08-26 12:09:36 +0600

    [done] journey section completed

--- src/pages/Landing/LandingPages.tsx
+++ src/pages/Landing/LandingPages.tsx
@@ -1,5 +1,6 @@
 import FeaturesFacilities from "./features/FeaturesFacilities";
 import Header from "./Headers/Header";
+import Journey from "./journey/Journey";
 import Services from "./Services/Services";
 import SiteBanner from "./siteBanner/SiteBanner";
 import Testimonials from "./Testimonials/Testimonials";
@@ -14,6 +15,7 @@ const LandingPages = () => {
       <Services />
       <SiteBanner />
       <Testimonials />
+      <Journey />
     </div>
   );
 };
---
+++ src/pages/Landing/journey/Journey.tsx
@@ -0,0 +1,81 @@
+import { CiCircleMore } from "react-icons/ci";
+import { FaCircleCheck } from "react-icons/fa6";
+import { LuUserCog2 } from "react-icons/lu";
+
+const Journey = () => {
+  return (
+    <div className="max-w-6xl mx-auto py-20 grid grid-cols-1 md:grid-cols-2 gap-20">
+      <div>
+        <h2 className="text-4xl font-bold text-[#333]">
+          Start Your Journey With{" "}
+          <span className="primary-liner ">Dreamsports</span> Badminton Today.
+        </h2>
+        <p className="text-sm my-6">
+          At DreamSports Badminton, we prioritize your satisfaction and value
+          your feedback as we continuously improve and evolve our learning
+          experiences.
+        </p>
+        <p className="text-sm ">
+          Our instructors utilize modern methods for effective badminton
+          lessons, offering introductory sessions for beginners and personalized
+          development plans to foster individual growth.
+        </p>
+
+        <div className="mt-8">
+          <h2 className="font-semibold text-[#333]">
+            Stay Ahead With Our Innovative Approach:
+          </h2>
+          <div className="flex items-start justify-between my-6 mb-8">
+            <ul className="space-y-3">
+              <li className="flex items-center gap-2">
+                <FaCircleCheck className="text-[#333] text-xl" />
+                <span>Skilled Professionals</span>
+              </li>
+              <li className="flex items-center gap-2">
+                <FaCircleCheck className="text-xl text-[#333]" />
+                <span>Modern Techniques</span>
+              </li>
+              <li className="flex items-center gap-2">
+                <FaCircleCheck className="text-xl text-[#333]" />
+                <span>Intro Lesson</span>
+              </li>
+            </ul>
+            <ul className="space-y-3">
+              <li className="flex items-center gap-2">
+                <FaCircleCheck className="text-xl text-[#333]" />
+                <span>Personal Development</span>
+              </li>
+              <li className="flex items-center gap-2">
+                <FaCircleCheck className="text-xl text-[#333]" />
+                <span>Advanced Equipment</span>
+              </li>
+              <li className="flex items-center gap-2">
+                <FaCircleCheck className="text-xl text-[#333]" />
+                <span>Interactive Classes For Easy Learning.</span>
+              </li>
+            </ul>
+          </div>
+          <div className=" flex items-center gap-6">
+            <button className="btn bg-[#097e52] text-white border-[#097e52] hover:bg-[#333] hover:text-white hover:border-[#333]">
+              <LuUserCog2 className="text-lg" />
+              Join With Us
+            </button>
+            <button className="btn bg-[#333] text-white border-[#333] hover:bg-[#097e52] hover:text-white hover:border-[#097e52]">
+              <CiCircleMore className="text-lg" />
+              Learn More
+            </button>
+          </div>
+        </div>
+      </div>
+      <div>
+        <img
+          className="w-full h-full"
+          src="https://dreamsports.dreamstechnologies.com/react/template/assets/img/journey-01.png"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Journey;
